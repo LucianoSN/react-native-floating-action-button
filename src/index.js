629d5396858ab37c462148584f1780f45accab98
Luciano Nery <lsnery@gmail.com>
2020-02-19 17:48:01 -0300

    fix a problem

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const Index = () => {
     return (
         <View style={styles.container}>
-            <Text> Open up App.js to start working on your app! </Text>{' '}
+            <Text> Open up App.js to start working on your app! </Text>
         </View>
     );
 };
