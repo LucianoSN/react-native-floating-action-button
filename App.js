2eeee70651d5885bc457ad92a6c7800e19c65aa9
Luciano Nery <lsnery@gmail.com>
2020-02-19 17:43:44 -0300

    adjusting structure

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text> Open up App.js to start working on your app! </Text>{' '}
-        </View>
-    );
-}
+import Index from './src';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+const App = () => {
+    return <Index />;
+};
+
+export default App;
